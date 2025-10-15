00eb8943117c8a892309e0304d5349cc79432dcd
edzamo <edzamo13@gmail.com>
2025-10-15 13:05:06 -0500

    first commit

--- my-proyect-with-v20/src/app/app.ts
+++ my-proyect-with-v20/src/app/app.ts
@@ -5,9 +5,14 @@ import { ApplicationInfo } from "./application-info/application-info";
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ApplicationInfo],
-  templateUrl: './app.html',
+  //templateUrl: './app.html',
+  template: `<h1>{{ title() }}</h1>
+    <h2>Welcome {{ name }}</h2>
+    `,
+      
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-proyect-with-v20');
+  name = 'Ewin';
 }
