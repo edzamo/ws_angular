462a69627c0487d38d92e590610e6a8345aae28f
edzamo <edzamo13@gmail.com>
2025-10-24 16:37:51 -0500

    adding readme  with information angular

--- my-proyect-with-v20/src/app/components/dragonball/service/dragonball-service.ts
+++ my-proyect-with-v20/src/app/components/dragonball/service/dragonball-service.ts
@@ -1,19 +1,37 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../../interfaces/character';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DragonballService {
-  constructor() {}
+
 
   characteres = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8500 },
   ]);
 
-  addCharacter($event: Character) {
+
+  saveLocalStorage= effect(() => {
+    console.log(`character count: ${this.characteres().length}`);
+
+    localStorage.setItem('characteres', JSON.stringify(this.characteres()));
+   console.log(`read characters: ${loadFromLocalStorage()}`);
+
+    
+  });
+  
+
+  addCharacter(character: Character) {
     // Method to add a new character. how works this? receives the event emitted from the child component
-    this.characteres.update((characters) => [...characters, $event]);
+    this.characteres.update((characters) => [...characters, character]);
   }
+}
+
+function loadFromLocalStorage(): Character[] {
+
+  const data = localStorage.getItem('characteres');
+  return data ? JSON.parse(data) : [];
+  
 }
